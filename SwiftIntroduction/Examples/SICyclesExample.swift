69426e55c1326a4604cbf7af2936ff5745301d34
Dmytro Bulaienko <dm.bulaienko@gmail.com>
2015-09-18 12:32:50 +0300

    provide some examples base

---
+++ SwiftIntroduction/Examples/SICyclesExample.swift
@@ -0,0 +1,33 @@
+//
+//  SICyclesExample.swift
+//  SwiftIntroduction
+//
+//  Created by iOS Developer on 9/18/15.
+//  Copyright (c) 2015 Applikey Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SICyclesExample: NSObject {
+    let values = [1, 25, 40, 9, 17, 8]
+    
+    // MARK: - Overloading
+   
+    func print() {
+        
+    }
+    
+    func print(arg: Int) {
+        println("value \(arg)")
+    }
+    
+    func print(args :Int...) {
+        for arg in args {
+            self.print(arg)
+        }
+    }
+    
+    // MARK: - Cycles
+    
+    
+}
---
+++ SwiftIntroduction/Examples/SIPropertiesAndSubscriptsExample.swift
@@ -0,0 +1,13 @@
+//
+//  SIPropertiesAndSubscriptsExample.swift
+//  SwiftIntroduction
+//
+//  Created by iOS Developer on 9/18/15.
+//  Copyright (c) 2015 Applikey Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SIPropertiesAndSubscriptsExample: NSObject {
+   
+}
